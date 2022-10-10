4ab9a52b043b0f8bf38ad9df1ce5707b2c4536c8
pawelwojcik <96476926+pawelwuuu@users.noreply.github.com>
2022-10-10 20:24:28 +0200

    Delete src directory

--- src/main/java/tracker/Exceptions/IllegalArgumentPassedException.java
+++
@@ -1,7 +0,0 @@
-package main.java.tracker.Exceptions;
-
-public class IllegalArgumentPassedException extends Exception{
-    public IllegalArgumentPassedException(String message){
-        super(message);
-    }
-}
--- src/main/java/tracker/Exceptions/UnknownCommandException.java
+++
@@ -1,7 +0,0 @@
-package main.java.tracker.Exceptions;
-
-public class UnknownCommandException extends Exception{
-    public UnknownCommandException(String message) {
-        super(message);
-    }
-}
